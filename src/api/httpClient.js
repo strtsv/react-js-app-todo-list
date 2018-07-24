b290b9c1407d582ba3ea7700a3147bb01aee6a5f
jsusilo <enr@bk.ru>
2018-07-24 19:55:52 +0500

    Set up the httpClient by axios

---
+++ src/api/httpClient.js
@@ -0,0 +1,27 @@
+import axios from 'axios' 
+
+const post = (url = '', data = '', config = {}) => {
+    return axios.post(url, data, config)
+}
+
+const get = (url) => {
+    return axios(url)
+}
+
+const put = (url = '', data = '', config = {}) => {
+    return axios.put(url, data, config)
+}
+
+const del = (url = '', config = {}) => {
+    return axios.delete(url, config)
+}
+
+const HttpClient = {
+    post,
+    get,
+    put,
+    delete: del
+}
+
+export {HttpClient}
+
--- src/index.js
+++ src/index.js
@@ -4,5 +4,8 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import 'semantic-ui-css/semantic.min.css';
+import 'font-awesome/css/font-awesome.css'
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
